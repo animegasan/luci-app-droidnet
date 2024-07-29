a4404a4d4cc968a7863ef3bfe8af2bec0b651902
Hilman Maulana <hilman0.0maulana@gmail.com>
2024-07-29 14:32:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
-file_name="luci-app-droidnet_1.1_all.ipk"
-version="v1.1"
+apk_name="luci-app-droidnet"
+version="1.1"
 
-echo "Updating OPKG"
-opkg update
-echo "Installing curl"
-opkg install curl
-echo "Downloading $file_name"
-curl -LO "https://github.com/animegasan/luci-app-droidnet/releases/download/$version/$file_name"
-echo "Installing $file_name"
-opkg install $file_name
-echo "Process completed. $file_name has been installed."
+echo "Updating packages"
+if ! opkg update; then
+    echo "Failed to update OPKG"
+    exit 1
+fi
+
+echo "Downloading $apk_name"
+if ! curl -LO "https://github.com/animegasan/$apk_name/releases/download/$version/${apk_name}_${version}_all.ipk"; then
+    echo "Failed to download $apk_name"
+    exit 1
+fi
+
+echo "Installing $apk_name"
+if ! opkg install "${apk_name}_${version}_all.ipk"; then
+    echo "Failed to install $apk_name"
+    exit 1
+fi
+
+echo "Process completed. $apk_name has been installed."
